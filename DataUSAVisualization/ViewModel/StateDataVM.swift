71088bdd2d12c9200162c1562ee6c1e42b6c56f3
Filipe Ramos <pandinus.imperator@gmail.com>
2024-08-10 10:04:15 +0100

    Added state assets and View Model

--- DataUSAVisualization/ViewModel/StateDataVM.swift
+++ DataUSAVisualization/ViewModel/StateDataVM.swift
@@ -1,35 +1,32 @@
 //
-//  NationalDataVM.swift
+//  StateDataVM.swift
 //  DataUSAVisualization
 //
-//  Created by Filipe Ramos on 09/08/2024.
+//  Created by Filipe Ramos on 10/08/2024.
 //
 
 import Foundation
 import Observation
 
 @Observable
-final class NationalDataVM {
+final class StateDataVM {
     @ObservationIgnored
     let service: DataUsaService
     
     var loading = true
-    var nationalData = [NationalData]()
+    var stateData = [StateData]()
     
     init(service: DataUsaService) {
         self.service = service
     }
 
     @MainActor
-    func fetchPopulationData() async {
+    func fetchStateData() async {
         do {
             loading = true
             defer { loading = false }
 
-            let data = try await service.getNationPopulation()
-                .compactMap { $0.yearDate != nil ? $0 : nil }
-
-            nationalData = Array(data.prefix(10))
+            stateData = try await service.getStatePopulation()
         } catch {
             #warning("Show error dialog")
             print(error)
