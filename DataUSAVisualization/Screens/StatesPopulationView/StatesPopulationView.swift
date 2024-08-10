19fc9890b57a33e8f85d4c57f0dac9f307ecaa47
Filipe Ramos <pandinus.imperator@gmail.com>
2024-08-10 11:56:25 +0100

    Added sorting to state list

--- DataUSAVisualization/Screens/StatesPopulationView/StatesPopulationView.swift
+++ DataUSAVisualization/Screens/StatesPopulationView/StatesPopulationView.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct StatesPopulationView: View {
     private let screenTitle = "Population by State"
-
+    
     @State private var vm = StateDataVM(service: DataUsaService())
-
+    
     var body: some View {
         NavigationStack {
-            if vm.loading {
-                ActivityIndicatorView(style: .large, tint: .accent)
-                    .navigationTitle(screenTitle)
-            } else {
-                ScrollView {
-                    Text("Year: \(vm.stateData.first?.year ?? "unknown")")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.bottom, 24)
-                        .padding(.horizontal)
-
-                    LazyVStack {
-                        ForEach(vm.stateData) { stateData in
-                            StateRow(stateData: stateData)
+            Group {
+                if vm.loading {
+                    ActivityIndicatorView(style: .large, tint: .accent)
+                } else {
+                    ScrollView {
+                        Text("Year: \(vm.stateData.first?.year ?? "unknown")")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.bottom, 24)
+                            .padding(.horizontal)
+                        
+                        LazyVStack {
+                            ForEach(vm.stateData) { stateData in
+                                StateRow(stateData: stateData)
+                            }
                         }
                     }
+                    .scrollIndicators(.hidden)
                 }
-                .scrollIndicators(.hidden)
-                .scrollBounceBehavior(.basedOnSize)
-                .padding(.vertical)
-                .navigationTitle(screenTitle)
+            }
+            .navigationTitle(screenTitle)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: vm.sortIcon)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(.accent)
+                        .onTapGesture {
+                            vm.switchSortingOrder()
+                        }
+                }
             }
         }
         .task {
--- DataUSAVisualization/ViewModel/StateDataVM.swift
+++ DataUSAVisualization/ViewModel/StateDataVM.swift
@@ -8,13 +8,26 @@
 import Foundation
 import Observation
 
+enum SortOrder: String {
+    case alphabetically
+    case populationSize
+}
+
 @Observable
 final class StateDataVM {
     @ObservationIgnored
     let service: DataUsaService
     
     var loading = true
+    var sortOrder: SortOrder = .alphabetically {
+        didSet {
+            sortData()
+        }
+    }
     var stateData = [StateData]()
+    var sortIcon: String {
+        sortOrder == .populationSize ? "square.and.arrow.up.on.square.fill" : "a.circle.fill"
+    }
     
     init(service: DataUsaService) {
         self.service = service
@@ -28,14 +41,33 @@ final class StateDataVM {
 
             stateData = try await service.getStatePopulation()
             calculateRelativePopulations()
+            sortData()
         } catch {
             #warning("Show error dialog")
             print(error)
         }
     }
     
+    func switchSortingOrder() {
+        sortOrder = switch sortOrder {
+        case .alphabetically:
+            .populationSize
+        case .populationSize:
+            .alphabetically
+        }
+    }
+    
     func calculateRelativePopulations() {
         let maxPopulation: Double = Double(stateData.max { $0.population < $1.population }?.population ?? 1)
         stateData.indices.forEach { stateData[$0].relativePopulation = Double(stateData[$0].population) / maxPopulation }
+    }
+    
+    func sortData() {
+        switch sortOrder {
+        case .alphabetically:
+            stateData = stateData.sorted { $0.name < $1.name }
+        case .populationSize:
+            stateData = stateData.sorted { $0.population > $1.population }
+        }
     }
 }
